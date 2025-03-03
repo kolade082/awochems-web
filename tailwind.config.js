1bd094ccfa7627c8a9b56be5af348652c6ad693e
kolade082 <koladeoluwadara@gmail.com>
2025-03-03 21:42:22 +0000

    tailwind issue solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
-module.exports = {
-    content: [
-      "./src/**/*.{html,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#1DA1F2", // Add a custom color
-        },
-        fontFamily: {
-          sans: ["Inter", "sans-serif"], // Add a custom font
-        },
-      },
-    },
-    plugins: [],
-  };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
