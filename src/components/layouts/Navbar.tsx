b66a074266d51caeba7fd669f1213c6b74f37221
kolade082 <koladeoluwadara@gmail.com>
2025-03-04 20:05:50 +0000

    tailwind and css conflict

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,88 +1,88 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {FaBars} from "react-icons/fa";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import Logo from "../../assets/logos/awo-logo-white.png";
-import '../../assets/styles/NavbarStyles.css'
+import "../../assets/styles/NavbarStyles.css";
 
 function Navbar() {
-    const [isNavExpanded, setIsNavExpanded] = useState(false);
-    const [isProductsDropdownOpen, setIsProductsDropdownOpen] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
+  return (
+    <nav className="relative flex w-full navbar-dark-grey py-2 shadow-md shadow-black/5">
+      <div className="container mx-auto px-4 flex flex-col lg:flex-row items-center justify-between">
+      <div className="w-full flex items-center justify-between">
+        <Link to="/" className="flex items-center">
+          <img
+            src={Logo}
+            style={{ height: "30px" }}
+            alt="awochem Logo"
+            loading="lazy"
+          />
+        </Link>
+        {/* Hamburger Menu Icon */}
+        <button
+          onClick={() => setIsNavExpanded(!isNavExpanded)}
+          className="hamburger lg:hidden"
+        >
+          <FaBars size={20} />
+        </button>
+        </div>
 
-    return (
-        <nav className="relative flex w-full justify-between navbar-dark-grey py-2 shadow-md shadow-black/5">
-            <div className="flex w-full justify-between px-3 lg:px-4">
-                {/* Hamburger Menu Icon */}
-                <button onClick={() => setIsNavExpanded(!isNavExpanded)} className="hamburger lg:hidden">
-                    <FaBars size={20}/>
-                </button>
-                {/* Logo */}
-                <Link to="/" className="flex items-center">
-                    <img
-                        src={Logo}
-                        style={{height: "30px"}}
-                        alt="awochem Logo"
-                        loading="lazy"
-                    />
-                </Link>
-
-
-                <ul className={`navbar-menu ${isNavExpanded ? 'navbar-block' : 'navbar-hidden'} lg:flex items-center lg:mr-auto`}>
-                    <li style={{marginLeft: "2em", marginRight: "2em"}}>
-                        <Link
-                            to="/"
-                            className="text-neutral-500 hover:text-neutral-700"
-                        >
-                            HOME
-                        </Link>
-                    </li>
-                    <div className="dropdown">
-                        <button
-                            onClick={() => setIsProductsDropdownOpen(!isProductsDropdownOpen)}
-                            className="text-white"
-                        >
-                            OUR PRODUCTS
-                        </button>
-
-                        {isProductsDropdownOpen && (
-                            <ul className="dropdown-menu">
-                                <li><Link to="/product/category1">Specialist Bag</Link></li>
-                                <li><Link to="/product/category2">Tray & Dolav Liners</Link></li>
-                                <li><Link to="/product/category3">Sheet & Tubing</Link></li>
-                                <li><Link to="/product/category4">Specialist Sacks</Link></li>
-                                {/* More categories */}
-                            </ul>
-                        )}
-                    </div>
-                    <li style={{marginLeft: "2em", marginRight: "2em"}}>
-                        <Link
-                            to="/sector"
-                            className="text-neutral-500 hover:text-neutral-700"
-                        >
-                            OUR SECTORS
-                        </Link>
-                    </li>
-                    <li style={{marginLeft: "2em", marginRight: "2em"}}>
-                        <Link
-                            to="/contact"
-                            className="text-neutral-500 hover:text-neutral-700"
-                        >
-                            CONTACT US
-                        </Link>
-                    </li>
-                    <li style={{marginLeft: "2em", marginRight: "2em"}}>
-                        <Link
-                            to="/about"
-                            className="text-neutral-500 hover:text-neutral-700"
-                        >
-                            ABOUT
-                        </Link>
-                    </li>
-                </ul>
-                
-            </div>
-        </nav>
-    );
+        <ul
+          className={`
+          absolute lg:static
+          top-full left-0 w-full
+          flex flex-col lg:flex-row 
+          items-center justify-center
+          bg-black lg:bg-transparent
+          space-y-4 lg:space-y-0 lg:space-x-6
+          ${isNavExpanded ? "block" : "hidden"} 
+          lg:flex
+          lg:absolute lg:left-1/2 
+          lg:transform lg:-translate-x-1/2
+        `}
+        >
+          <li>
+            <Link
+              to="/"
+              className="text-white hover:text-neutral-300 transition-colors"
+            >
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="text-neutral-500 hover:text-neutral-700">
+              OUR PRODUCTS
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/sector"
+              className="text-neutral-500 hover:text-neutral-700"
+            >
+              OUR SECTORS
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="text-neutral-500 hover:text-neutral-700"
+            >
+              CONTACT US
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="text-neutral-500 hover:text-neutral-700"
+            >
+              ABOUT
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
 }
 
